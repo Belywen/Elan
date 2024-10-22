320ede64eca3b58f79f2d11791c4ca50f8f8454e
Laury <112404773+Belywen@users.noreply.github.com>
2024-10-22 16:56:13 +0200

    maj

--- ALGOPHP/Partie1/exo04.php
+++ ALGOPHP/Partie1/exo04.php
@@ -1,16 +1,20 @@
 <h2>Exercice 4</h2>
 
 <?php
-    $phrase = "engage le jeu que je le gagne";
+    $phrase = "Engage le jeu que je le gagne";
+    echo $phrase;
+    echo "</br></br>";
 
-    $palin = str_replace(" ", "", $phrase);
+    $phraseMin = strtolower($phrase);
+
+    $palin = str_replace(" ", "", $phraseMin);
     $nilap = strrev($palin);
     
 
     echo $palin;
-    echo "</br>";
+    echo "</br></br>";
     echo $nilap;
-    echo "</br>";
+    echo "</br></br>";
 
     if ($palin == strrev($palin)) {
         echo $phrase." est un palindrome.";
--- ALGOPHP/Partie1/exo07.php
+++ ALGOPHP/Partie1/exo07.php
@@ -5,13 +5,13 @@
 
     echo "Affichage : ";
     echo "</br></br>";
-    
-    if ($age >= 6 && $age <= 7) {
-        echo "L'enfant qui a ".$age." ans appartient à la catégorie 'Poussin'";
-    } else if ($age >= 8 && $age <= 9) {
-        echo "L'enfant qui a ".$age." ans appartient à la catégorie 'Pupille'";
-    } else if ($age >= 12) {
+
+    if ($age >= 12) {
         echo "L'enfant qui a ".$age." ans appartient à la catégorie 'Cadet'";
+    } else if ($age >= 8 && $age <= 9) {
+            echo "L'enfant qui a ".$age." ans appartient à la catégorie 'Pupille'";    
+    } else if ($age >= 6 && $age <= 7) {
+        echo "L'enfant qui a ".$age." ans appartient à la catégorie 'Poussin'";
     } else {
         echo "L'enfant qui a ".$age." n'entre dans aucune catégorie.";
     }
